b0ba58deb390234baab08054a2897f499cc0d7a9
mar1 <marwanechampain@icloud.com>
2023-12-05 14:32:23 +0100

    update parse_card and some memory issue

--- day-04/src/bin/part1.rs
+++ day-04/src/bin/part1.rs
@@ -2,10 +2,15 @@ use std::fs;
 
 fn parse_card(card: &str) -> (Vec<i32>, Vec<i32>) {
     let parts: Vec<&str> = card.split(" | ").collect();
-    let winning_numbers = parts[0].split_whitespace().map(|num| num.parse::<i32>().unwrap()).collect();
-    let user_numbers = parts[1].split_whitespace().map(|num| num.parse::<i32>().unwrap()).collect();
+    let winning_numbers = parts[0].split_whitespace()
+                                  .map(|num| num.trim().parse::<i32>().unwrap())
+                                  .collect();
+    let user_numbers = parts[1].split_whitespace()
+                                .map(|num| num.trim().parse::<i32>().unwrap())
+                                .collect();
     (winning_numbers, user_numbers)
 }
+
 
 fn calculate_card_points(winning_numbers: &Vec<i32>, user_numbers: &Vec<i32>) -> i32 {
     let mut points = 0;
@@ -27,7 +32,16 @@ fn total_points(cards: &Vec<(Vec<i32>, Vec<i32>)>) -> i32 {
          .sum()
 }
 
-#[cfg(test)]:
+fn main() {
+    let input = fs::read_to_string("input.txt").expect("Failed to read file");
+    let cards: Vec<(Vec<i32>, Vec<i32>)> = input.lines().map(|line| parse_card(line)).collect();
+
+    let total = total_points(&cards);
+
+    println!("Total points: {}", total);
+}
+
+#[cfg(test)]
 mod tests {
     use super::*;
 
@@ -36,16 +50,7 @@ mod tests {
         let input = fs::read_to_string("inputtest.txt").expect("Failed to read file");
         let cards: Vec<(Vec<i32>, Vec<i32>)> = input.lines().map(|line| parse_card(line)).collect();
 
-        let total = total_points(cards);
+        let total = total_points(&cards);
         assert_eq!(total, 13);
     }
 } 
-
-fn main() {
-    let input = fs::read_to_string("input.txt").expect("Failed to read file");
-    let cards: Vec<(Vec<i32>, Vec<i32>)> = input.lines().map(|line| parse_card(line)).collect();
-
-    let total = total_points(cards);
-
-    println!("Total points: {}", total);
-}
